ae8537fe4ab4b62d50dd2f6a236e8d17fb6e216f
Anton <antonstromkarl@gmail.com>
2019-10-30 09:58:52 +0100

    Clean up and some configurations for client

--- core/src/com/mygdx/game/config/GameConfig.java
+++ core/src/com/mygdx/game/config/GameConfig.java
@@ -1,4 +1,10 @@
 package com.mygdx.game.config;
 
 public class GameConfig {
+
+    public static final float WIDTH = 1920f;
+    public static final float HEIGHT = 1080f;
+
+    public static final boolean FORCE_EXIT_FALSE = false;
+
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,10 +3,14 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.BravehearthGame;
+import com.mygdx.game.config.GameConfig;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = (int) GameConfig.WIDTH;
+		config.height = (int) GameConfig.HEIGHT;
+		config.forceExit = GameConfig.FORCE_EXIT_FALSE;
 		new LwjglApplication(new BravehearthGame(), config);
 	}
 }
