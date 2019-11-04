2bc83ee43be39b0c02442f60c6514becaff9ab35
Ted <ted.agner@hotmail.com>
2019-11-04 15:51:42 +0100

    Fixed avatar speed

--- server/src/main/java/network/networkMessages/avatar/Avatar.java
+++ server/src/main/java/network/networkMessages/avatar/Avatar.java
@@ -5,8 +5,8 @@ import network.networkMessages.CharacterClass;
 import java.util.UUID;
 
 public class Avatar {
-    private float maxXspeed = 2f;
-    private float maxYspeed = 2f;
+    private float maxXspeed = 0.03f;
+    private float maxYspeed = 0.03f;
 
     private float boundsRadius;
     private float size;
