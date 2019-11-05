935a4120e4e1fde7cfeda989b2968fe595d3e808
Kate <curran.kate.t@gmail.com>
2019-11-05 12:06:49 +0100

    set-up DB stuff on server

---
+++ server/src/main/java/database/DBQueries.java
@@ -0,0 +1,25 @@
+package database;
+
+import network.networkMessages.User;
+
+import java.sql.*;
+
+public abstract class DBQueries {
+
+    public static PreparedStatement prep(String SQLQuery) {
+        return Database.getInstance().prepareStatement(SQLQuery);
+    }
+
+    public static User getMatchingUser(String username, String password) {
+        User result = null;
+        PreparedStatement ps = prep("SELECT * FROM users WHERE UserLogIn = ? AND UserPass = ?");
+        try {
+            ps.setString(1, username);
+            ps.setString(2, password);
+            result = (User) new ObjectMapper<>(User.class).mapOne(ps.executeQuery());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
---
+++ server/src/main/java/database/Database.java
@@ -0,0 +1,39 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+public class Database {
+
+    private static Database dbInstance = new Database();
+
+    public static Database getInstance() {
+        return dbInstance;
+    }
+
+    private Database() { connectToDb(); }
+
+    //final String connectionURL = ;
+    private Connection conn = null;
+    private HashMap<String, PreparedStatement> preparedStatements = new HashMap<>();
+
+    public PreparedStatement prepareStatement(String SQLQuery){
+        PreparedStatement ps = preparedStatements.get(SQLQuery);
+        if (ps == null) {
+            try { ps = conn.prepareStatement(SQLQuery); }
+            catch (SQLException e) { e.printStackTrace(); }
+        }
+        return ps;
+    }
+
+   private void connectToDb() {
+        /*try {
+            conn = DriverManager.getConnection(connectionURL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }*/
+    }
+}
