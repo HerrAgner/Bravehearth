14ca41a9c8020dd4dcd3c7367fa5c01d090450a2
Kate <curran.kate.t@gmail.com>
2019-11-06 09:38:06 +0100

    query for getUserAvatar works

--- server/src/main/java/handlers/CommandHandler.java
+++ server/src/main/java/handlers/CommandHandler.java
@@ -58,7 +58,6 @@ public class CommandHandler {
         Connection c = entry.getKey();
         if (o instanceof Login) {
             User user = (createUser(o));
-            System.out.println("avatar name " + user.getAvatar().getName());
             if (user != null) {
                 server.sendToTCP(connection.getID(), user);
                 auh.addToActiveUsers(connection.getID(), user);
@@ -102,22 +101,12 @@ public class CommandHandler {
 
     private User createUser(Object object) {
         Login loginObject = (Login) object;
-        Avatar avatar = new Avatar();
-        /*avatar.setCharacterClass(CharacterClass.DUMMYCLASS);
-        avatar.setX(10);
-        avatar.setY(10);
-        avatar.setMaxHealth(30);
-        avatar.setAttackDamage(1);
-        avatar.setAttackSpeed((float) 1.5);
-        avatar.setAttackRange((float) 1.5);
-        avatar.setHealth(avatar.getMaxHealth());
-        avatar.setId(UUID.randomUUID());*/
 
         User user = DBQueries.getMatchingUser(loginObject.getUsername(), loginObject.getPassword());
         try {
             user.setAvatar(DBQueries.getUserAvatar(user.getId()));
         } catch (NullPointerException e) {
-            System.out.println("No avatar found on user.");
+            System.out.println("No avatar found for user.");
         }
         return user;
     }
