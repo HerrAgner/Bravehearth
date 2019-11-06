635e70f700e236ec69cfe206e22672bc8e3c93fd
Ted <ted.agner@hotmail.com>
2019-11-06 19:25:15 +0100

    Added delta-time to movement on server

--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -2,6 +2,11 @@ import database.DBQueries;
 import network.NetworkListener;
 import game.GameServer;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class ServerMain {
     public static void main(String[] args) {
         GameServer.getInstance();
--- server/src/main/java/game/GameLoop.java
+++ server/src/main/java/game/GameLoop.java
@@ -6,6 +6,8 @@ import handlers.MovementHandler;
 import network.networkMessages.avatar.Avatar;
 import network.networkMessages.Position;
 
+import java.util.Timer;
+
 public class GameLoop implements Runnable {
     private boolean running;
 
@@ -15,12 +17,15 @@ public class GameLoop implements Runnable {
 
     @Override
     public void run() {
+        long prevtime = System.currentTimeMillis();
+        System.out.println(System.currentTimeMillis());
 
         while (running) {
+            float delta = (float) ((System.currentTimeMillis() - prevtime) / 1000.0);
 
             MovementHandler.movementLoopList.forEach((key, value) ->
                     value.forEach(movement -> {
-                        Position pos = updatePosition(GameServer.getInstance().aa.get(GameServer.getInstance().au.get(key.getID()).getAvatar().getId()), movement);
+                        Position pos = updatePosition(GameServer.getInstance().aa.get(GameServer.getInstance().au.get(key.getID()).getAvatar().getId()), movement, delta);
                         GameServer.getInstance().aa.get(GameServer.getInstance().au.get(key.getID()).getAvatar().getId()).setX(pos.getX());
                         GameServer.getInstance().aa.get(GameServer.getInstance().au.get(key.getID()).getAvatar().getId()).setY(pos.getY());
                         GameServer.getInstance().getServer().sendToAllUDP(pos);
@@ -34,8 +39,8 @@ public class GameLoop implements Runnable {
                 }
 
             }
-
             try {
+                prevtime = System.currentTimeMillis();
                 Thread.sleep(16);
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -45,20 +50,20 @@ public class GameLoop implements Runnable {
 
     }
 
-    public Position updatePosition(Avatar avatar, Movement movement) {
+    public Position updatePosition(Avatar avatar, Movement movement, float delta) {
         Position position;
         switch (movement) {
             case FORWARD:
-                position = new Position(avatar.getX(), avatar.getY() + avatar.getMaxYspeed(), avatar.getId());
+                position = new Position(avatar.getX(), avatar.getY() + avatar.getMaxYspeed() * delta, avatar.getId());
                 break;
             case BACKWARD:
-                position = new Position(avatar.getX(), avatar.getY() - avatar.getMaxYspeed(), avatar.getId());
+                position = new Position(avatar.getX(), avatar.getY() - avatar.getMaxYspeed() * delta, avatar.getId());
                 break;
             case LEFT:
-                position = new Position(avatar.getX() - avatar.getMaxXspeed(), avatar.getY(), avatar.getId());
+                position = new Position(avatar.getX() - avatar.getMaxXspeed() * delta, avatar.getY(), avatar.getId());
                 break;
             case RIGHT:
-                position = new Position(avatar.getX() + avatar.getMaxXspeed(), avatar.getY(), avatar.getId());
+                position = new Position(avatar.getX() + avatar.getMaxXspeed() *delta, avatar.getY(), avatar.getId());
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + movement);
