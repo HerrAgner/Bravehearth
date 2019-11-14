51e9764d70dd4cb5f444d8863e5dc3f26ae43ba5
Anton <antonstromkarl@gmail.com>
2019-11-14 15:47:53 +0100

    added a layer on top of backpack

--- core/src/com/mygdx/game/screen/Inventory.java
+++ core/src/com/mygdx/game/screen/Inventory.java
@@ -1,20 +1,14 @@
 package com.mygdx.game.screen;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.viewport.ScalingViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
+
 
 import java.util.ArrayList;
 
@@ -22,39 +16,51 @@ public class Inventory {
     private Stage stage;
     private Skin skin;
     private TextureAtlas atlas;
-    private Image image;
     private Table table;
+    private Table itemSlots;
     private boolean isOpen;
     private ArrayList<Image> images;
+    private ArrayList<Image> itemSlot;
 
     public Inventory() {
         stage = new Stage();
         table = new Table();
         skin = new Skin();
+        itemSlots = new Table();
         atlas = new TextureAtlas(Gdx.files.internal("hud.atlas"));
         skin.addRegions(atlas);
         images = new ArrayList<>();
+        itemSlot = new ArrayList<>();
         isOpen = false;
-        for (int i = 0; i <30; i++){
-            if(i % 10 == 0){
+        for (int i = 0; i < 30; i++) {
+            if (i % 10 == 0) {
                 table.row();
+                itemSlots.row();
             }
-                Image image = new Image();
-                image.setDrawable(skin, "hudboxmiddlegray");
-                images.add(image);
-                image.addListener(new ClickListener(){
-                    @Override
-                    public void clicked(InputEvent event, float x, float y){
-                        System.out.println();
-                    }
-                });
-                table.add(image).width(60).height(60);
-            }
-            table.row();
-        table.setFillParent(true);
-        stage.addActor(table);
-        }
+            Image slot = new Image();
 
+            itemSlot.add(slot);
+            Image image = new Image();
+            image.setDrawable(skin, "hudboxmiddlegray");
+            images.add(image);
+            int finalI = i;
+            slot.addListener(new ClickListener() {
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    getBackPackSlot(finalI);
+
+                }
+            });
+            table.add(image).width(60).height(60);
+            itemSlots.add(slot).width(60).height(60);
+        }
+        table.row();
+        itemSlots.row();
+        table.setFillParent(true);
+        itemSlots.setFillParent(true);
+        stage.addActor(table);
+        stage.addActor(itemSlots);
+    }
 
 
     public Stage getStage() {
@@ -63,6 +69,10 @@ public class Inventory {
 
     public void toggleInventory() {
         isOpen = !isOpen;
+    }
+
+    public void getBackPackSlot(int i) {
+        System.out.println(itemSlot.get(i));
     }
 
     public boolean isOpen() {
