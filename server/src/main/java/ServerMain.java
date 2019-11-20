30981792774017794294dad32521b6f3e8eeef93
Kate <curran.kate.t@gmail.com>
2019-11-20 12:00:00 +0100

    db query save equipped items

--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -1,10 +1,6 @@
 import database.DBQueries;
 import game.GameServer;
 import network.NetworkListener;
-import network.networkMessages.items.Weapon;
-import network.networkMessages.items.Wearable;
-
-import java.util.Random;
 
 public class ServerMain {
     public static void main(String[] args) {
--- server/src/main/java/database/DBQueries.java
+++ server/src/main/java/database/DBQueries.java
@@ -384,4 +384,36 @@ public abstract class DBQueries {
             e.printStackTrace();
         }
     }
+
+    public static void saveToEquippedItems(int avatarId, EquippedItems eq) {
+        PreparedStatement ps = prep("UPDATE equippedItems SET headSlot = ?, chestSlot = ?, legSlot = ?, " +
+                "feetSlot = ?, weaponSlot = ?, offHandSlot = ?, accessorySlot = ? WHERE avatar = ?");
+        try {
+            if(eq.getEquippedItems().get(WearableType.HEAD).getId() > 0) { ps.setInt(1, eq.getEquippedItems().get(WearableType.HEAD).getId()); }
+            else ps.setString(1, null);
+
+            if(eq.getEquippedItems().get(WearableType.CHEST).getId() > 0) { ps.setInt(2, eq.getEquippedItems().get(WearableType.CHEST).getId()); }
+            else ps.setString(2, null);
+
+            if(eq.getEquippedItems().get(WearableType.LEGS).getId() > 0) { ps.setInt(3, eq.getEquippedItems().get(WearableType.LEGS).getId()); }
+            else ps.setString(3, null);
+
+            if(eq.getEquippedItems().get(WearableType.FEET).getId() > 0) { ps.setInt(4, eq.getEquippedItems().get(WearableType.FEET).getId()); }
+            else ps.setString(4, null);
+
+            if(eq.getEquippedItems().get(WearableType.WEAPON).getId() > 0) { ps.setInt(5, eq.getEquippedItems().get(WearableType.WEAPON).getId()); }
+            else ps.setString(5, null);
+
+            if(eq.getEquippedItems().get(WearableType.OFFHAND).getId() > 0) { ps.setInt(6, eq.getEquippedItems().get(WearableType.OFFHAND).getId()); }
+            else ps.setString(6, null);
+
+            if(eq.getEquippedItems().get(WearableType.ACCESSORY).getId() > 0) { ps.setInt(7, eq.getEquippedItems().get(WearableType.ACCESSORY).getId()); }
+            else ps.setString(7, null);
+
+            ps.setInt(8, avatarId);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
